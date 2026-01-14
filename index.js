f56b97b0f0b0a7d7832b0b2956681f226780feb5
GriffithS17 <228213971+GriffithS17@users.noreply.github.com>
2026-01-14 14:46:38 -0300

    order by threat level and show malicious and suspicious results at the top of the list

--- index.js
+++ index.js
@@ -13,12 +13,30 @@ app.get("/", (req, res) => {
 
 app.use(express.static("public"))
 
-
 function normalizeUrl(input) {
   if (!/^https?:\/\//i.test(input)) {
     return 'https://' + input
   }
   return input
+}
+
+function verifyThreatLevel(a){
+  var threatLevel
+
+  if (a == 'harmless' || a == 'clean'){
+    threatLevel = 3
+  }
+  else if (a == 'suspicious'){
+    threatLevel = 2
+  }
+  else if (a == 'undetected' || a == 'unrated'){
+    threatLevel = 4
+  }
+  else{
+    threatLevel = 1
+  }
+
+  return threatLevel
 }
 
 app.post("/upload", async (req, res) => {
@@ -34,15 +52,25 @@ app.post("/upload", async (req, res) => {
 
   try {
     const result = await axios.get(`https://www.virustotal.com/api/v3/domains/${domain}`, config)
-    console.log(JSON.stringify(result.data, null, 2))
+    console.log(result.data)
 
     const attributes = result.data.data.attributes
+
+    const vendor = attributes.last_analysis_results
+
+    const sortedVendors = Object.keys(vendor).sort((a, b) => {
+      const scoreA = verifyThreatLevel(vendor[a].result)
+      const scoreB = verifyThreatLevel(vendor[b].result)
+
+      return scoreA - scoreB
+    })
 
     res.render("index.ejs", {
       showResults: true,
       harmless: attributes.total_votes.harmless,
       malicious: attributes.total_votes.malicious,
-      vendor: attributes.last_analysis_results,
+      vendor: vendor,
+      sortedVendors: sortedVendors,
       name: result.data.data.id
     })
 
